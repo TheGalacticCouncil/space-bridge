3a5e5be4de8f22fbc5b773130eb7e91c9ff865d3
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-01-18 13:34:01 +0200

    minor clean up and a fix to AnalogReader read() function

--- HwReader/AnalogReader.py
+++ HwReader/AnalogReader.py
@@ -11,29 +11,31 @@ name.read()'''
 from RPi import GPIO            # Raspberry GPIO pins
 from gpiozero import MCP3008    # A/D converter
 
+
 class AnalogInput():
-    def __init__(self, inputChannel):        
+
+    def __init__(self, inputChannel):
         self.analogInput = MCP3008(inputChannel)
 
     def readRaw(self):
         return self.analogInput.value
-        
-    def read(decimals=2, minimum=0, maximum=100):
+
+    def read(self, decimals=2, minimum=0, maximum=100):
         'rounds to [decimals]'
         'and rescales the input from [minimum] to [maximum]'
-        
+
         # add rounding
-        
+
         # add scaling
-        
+
         return self.analogInput.value
-        
+
 # Module can be run directly to test its function
 if __name__ == "__main__":
     from time import sleep
-        
+
     input1 = AnalogInput(0)
-    
+
     try:
         while True:
             value = input1.readRaw()
--- HwReader/EncoderReader.py
+++ HwReader/EncoderReader.py
@@ -7,7 +7,9 @@ name.read([counter])'''
 from RPi import GPIO
 
 class EncoderInput():
+
     def __init__(self, clk, dt):
+
         self.clockPin = clk
         self.dtPin = dt
 
@@ -18,29 +20,32 @@ class EncoderInput():
         self.previousClockState = GPIO.input(self.clockPin)
 
     def read(self, counter=0):
+
         clockState = GPIO.input(self.clockPin)
         dtState = GPIO.input(self.dtPin)
+
         if clockState != self.previousClockState:
             if dtState != clockState:
                 counter += 1
             else:
                 counter -= 1
-        self.previousClockState = clockState    
-        
+        self.previousClockState = clockState
+
         return counter
+
 
 # Module can be run directly to test its function
 if __name__ == "__main__":
     from time import sleep
-    
+
     counter = 0      # Define counter
     clk = 17         # Define clock pin
     dt = 24          # define dt pin
-    
+
     delta = 0
-    
+
     input1 = EncoderInput(clk, dt)
-    
+
     try:
         while True:
             ###
@@ -57,13 +62,13 @@ if __name__ == "__main__":
 
 ##########
 ##########
-        
+
 ## configurable module for reading a rotary encoder input
 #'''
 #encoder(
 #  "clk" GPIO pin,
 #  "dt" GPIO pin,
-#  [a Counter to be incremented]) 
+#  [a Counter to be incremented])
 #Output:
 #  incremented counter or relative change
 #'''
