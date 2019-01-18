c964375c338da9820f9da02e66c6f270a10c1387
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-01-18 15:22:30 +0200

    improved documentation, created function for loading config files. Renamed delta() to change() due to conflict with built in function. Now HwReader repots only CHANGED values.

--- HwReader/HwReader.py
+++ HwReader/HwReader.py
@@ -1,20 +1,20 @@
 # J.V.Ojala 17.01.2019
 # HwReader
 
-from EncoderReader import EncoderInput
+from .EncoderReader import EncoderInput
 from RPi import GPIO
 
 # add: read config from file
 idleTime = 0.001
 
 
-def delta(value, counter=0, threshold=0):
+def change(value, counter=0, threshold=0, changed=False):
     """
-    value, [counter], [threshold]
-    input value
-    a counter that is incremented (optional, default=0)
-    threshold for incrementing the counter (optional, default=0)
-     -for digital inputs use zero
+    counter, changed = change(value, [counter], [threshold])
+     - input value
+     - a counter that is incremented (optional, default=0)
+     - threshold for incrementing the counter (optional, default=0)
+       > for digital inputs use zero
     """
 
     delta = value
@@ -22,10 +22,24 @@ def delta(value, counter=0, threshold=0):
     # If change is large enough to trigger an update
     # for digital inputs use zero
     if abs(delta) > threshold:
+        changed = True
         counter += delta
-        return(counter)
+        return counter, changed
+    else:
+        return counter, changed
+
+def loadConfig():
+    """
+    Reads the config file and inputs the parameters from it
+    """
+    configfile = open('config', 'r')
+
+    configfile.close()
+
+
 
 if __name__ == "__main__":
+
     from time import sleep
 
     counter = 0      # Define counter
@@ -36,20 +50,17 @@ if __name__ == "__main__":
 
     input1 = EncoderInput(clk, dt)
 
+
     try:
+
+        # Main Loop
         while True:
-#            thing = delta(input1.read(), counter)
-#            print(thing)
-#            sleep(0.001)
 
+            counter, changed = change(input1.read(), counter)
 
-            ###
-            delta = input1.read()
-            if delta != 0:
-                counter += delta
+            if changed == True:
                 print(counter)
-            delta = 0
-            ###
+
             sleep(idleTime)
 
     finally:
