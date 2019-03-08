028fa2e1dfae07fe9a53171999286d9f1a57577c
Tomi Jokkeenhaara <tomi.jokkeenhaara@itfox.fi>
2019-03-08 16:47:39 +0200

    Created requestSender, it is not implemented but used in index.ts.

--- ShipControl/src/index.ts
+++ ShipControl/src/index.ts
@@ -1,30 +1,37 @@
 import dgram from "dgram";
 import { AddressInfo } from "net";
 import { RequestCreator } from "./requestCreator";
+import { RequestSender } from "./requestSender";
 import validator from "event-validator";
-
 
 const server: dgram.Socket = dgram.createSocket("udp4");
 
 const requestCreator: RequestCreator = new RequestCreator();
+const requestSender: RequestSender = new RequestSender();
 
-server.on("error", (err) => {
-    console.log(`Server shutdown due to following error:\n ${err}`);
-    server.close;
+server.on("error", err => {
+  console.log(`Server shutdown due to following error:\n ${err}`);
+  server.close;
 });
 
 server.on("listening", () => {
-    const address: AddressInfo = server.address() as AddressInfo;
-    console.log(`Server listening to ${address.address}:${address.port}`);
+  const address: AddressInfo = server.address() as AddressInfo;
+  console.log(`Server listening to ${address.address}:${address.port}`);
 });
 
-server.on("message", (message) => {
-    validator.validateRawEvent(message)
-        .then(event => { requestCreator.handleEvent(event) })
-        .catch(err => {
-            console.log("Invalid event!");
-            console.log(err);
+server.on("message", message => {
+  validator
+    .validateRawEvent(message)
+    .then(event => {
+      requestCreator.handleEvent(event);
+    })
+    .then(request => {
+      requestSender.send(request, server);
+    })
+    .catch(err => {
+      console.log("Invalid event!");
+      console.log(err);
     });
-})
+});
 
 server.bind(41114);
---
+++ ShipControl/src/requestSender.ts
@@ -0,0 +1,5 @@
+import dgram from "dgram";
+
+export class RequestSender {
+  send(request: string, server: dgram.Socket) {}
+}
