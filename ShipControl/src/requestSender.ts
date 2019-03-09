587d31b95cd31cfca5962c8f8e4daa17a9e622a5
Tomi Jokkeenhaara <tomi.jokkeenhaara@itfox.fi>
2019-03-09 18:06:24 +0200

    Created requests are now sent to localhost:41114. Added temp implementation for SELECT_WEAPON.

--- ShipControl/src/requestSender.ts
+++ ShipControl/src/requestSender.ts
@@ -1,5 +1,9 @@
 import dgram from "dgram";
 
 export class RequestSender {
-  send(request: string, server: dgram.Socket) {}
+  send(request: string, server: dgram.Socket) {
+    server.send(request, 41114, "localhost", err => {
+      server.close();
+    });
+  }
 }
