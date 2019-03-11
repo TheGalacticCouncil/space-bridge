f68bcf5f1b4b09143d26d8e54a0ceb52046fa220
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-03-11 16:30:40 +0200

    First version of udpSender
    Tested to work on Windows and Linux (Raspian)

---
+++ HwReader/udpSender.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# J.V.Ojala 11.03.2019
+# UdpSender
+
+from time import sleep
+#import threading           # UdpSender is easyly threadable, but
+import socket               # on the one-core Pi-Zero-W it would
+from timeit import Timer    # likely be counterproductive here.
+
+#class UdpSender(threading.Thread):
+class UdpSender():
+
+    def __init__(self, broadcast_ip, port):
+        #threading.Thread.__init__(self)
+        self.broadcast_ip = broadcast_ip
+        self.port = port
+
+    def run(self, message):
+        byte_message = bytearray(message,"utf-8")
+        network = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        network.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        network.sendto(byte_message, (udpIP, udpPort))
+
+if __name__ == '__main__':
+
+    # udpIP = '127.0.0.1'
+    #udpIP = '0.0.0.0'
+    udpIP = '192.168.10.255'
+    #udpIP = '<broadcast>'
+
+    udpPort = 41114     #22100
+    #bufferSize = 508    #1024
+
+    message = "Howdy world!"
+    udpSender = UdpSender(udpIP, udpPort)
+    udpSender.run(message)
+
