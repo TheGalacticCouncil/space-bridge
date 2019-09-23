a5146bde8b56727c1b6c48ddd5b4285e512e1409
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-09-23 23:14:08 +0300

    Added a license message HwReader

--- HwReader/HwReader.py
+++ HwReader/HwReader.py
@@ -34,6 +34,23 @@ import_time = int((end_time - start_time)*1000)
 
 logger = Logger(__name__)
 
+license="""
+   HwReader Copyright 2019 JValtteri
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+print("\n\n\n\n"+license+"\n")
+
 logger.debug("Module imports compleate in %i ms" % import_time)
 
 start_time = time()
