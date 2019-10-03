32944da2ee6d42e02693c18163eae5979795697c
JValtteri <46611993+JValtteri@users.noreply.github.com>
2019-10-03 10:19:23 +0300

    Added run and setup scripts (untested)

---
+++ HwReader/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Setup sctipt for installing HwReader and it's dependancies
+
+echo "Installing Python 3"
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-pip
+
+echo "Installing libraries"
+pip3 install -r requirements.txt
+
+echo "================="
+echo -e "\nInstall complete!"
+echo "================="
+echo -e "\nYou can run Hwreader by running run.sh"
+
+echo -e "\n\nCreate a config.yml that matches your hardware configuration or"
+echo -e "copy one from config examples and name as config.yml"
