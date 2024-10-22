620a00408e7bba74a3c46fd141783dfdaec838c3
JValtteri <46611993+JValtteri@users.noreply.github.com>
2024-10-23 00:04:40 +0300

    Added color to setup script

--- HwReader/setup.sh
+++ HwReader/setup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 # Setup sctipt for installing HwReader and it's dependancies
 
-echo "Installing Python 3"
+# Terminal Colors
+no_color=$(echo -e "\033[0m")
+yellow=$(echo -e "\033[1;36m")
+green=$(echo -e "\033[1;32m")
+red=$(echo -e "\033[1;31m")
+
+echo -e "${yellow}Installing Python 3${no_color}"
 sudo apt-get update
 sudo apt-get install python3
 sudo apt-get install python3-pip
 
-echo "Update CA-certificates"
+echo -e "${yellow}Update CA-certificates${no_color}"
 apt-get install -y --reinstall ca-certificates
 
-echo "Installing libraries"
+echo -e "${yellow}Installing libraries${no_color}"
 pip3 install -r requirements.txt  --break-system-packages
 
 echo "================="
-echo -e "\nInstall complete!"
+echo -e "${green}\nInstall complete!${no_color}"
 echo "================="
 echo -e "\nYou can run Hwreader by running run.sh"
 
