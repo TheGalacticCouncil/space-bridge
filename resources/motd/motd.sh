480f31673cbd0cee7c26f05de729be1a75d3bc02
JValtteri <46611993+JValtteri@users.noreply.github.com>
2024-11-01 20:01:24 +0200

    Added configuration by enviroment variable

--- resources/motd/motd.sh
+++ resources/motd/motd.sh
@@ -5,6 +5,10 @@ green=$(echo -e "\033[1;32m")
 red=$(echo -e "\033[1;31m")
 cyan=$(echo -e "\033[1;36m")
 
+# The script expects an enviromental variable STATION. 
+# Define it in .bashrc or profile
+# export STATION=
+
 #RESOURCE_PATH=/etc/
 RESOURCE_PATH=~/motd/
 #RESOURCE_PATH=~/git/space-bridge/resources/motd/
@@ -15,9 +19,18 @@ RESOURCE_PATH=~/motd/
 "$RESOURCE_PATH"/texts/echo_space_bridge.sh
 
 # Station type
-#"$RESOURCE_PATH"/texts/echo_engineer_a.sh
-#"$RESOURCE_PATH"/texts/echo_engineer_b.sh
-#"$RESOURCE_PATH"/texts/echo_weapons.sh
+if [ "$STATION" == "engineer-a" ]; then
+    "$RESOURCE_PATH"/texts/echo_engineer_a.sh
+elif [ "$STATION" == "engineer-b" ]; then
+    "$RESOURCE_PATH"/texts/echo_engineer_b.sh
+elif [ "$STATION" == "weapons" ]; then
+    "$RESOURCE_PATH"/texts/echo_weapons.sh
+elif [ "$STATION" == "" ]; then
+    echo ""
+else
+    echo "Station:"
+    echo "    $STATION"
+fi
 
 # System info
 "$RESOURCE_PATH"/texts/echo_system_info.sh
